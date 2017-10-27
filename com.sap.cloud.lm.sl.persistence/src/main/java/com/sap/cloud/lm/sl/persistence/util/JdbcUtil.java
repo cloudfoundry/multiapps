3c7903976b7156c19e32bfd74d54702b8b680087
Aleksandar Tsvetkov <aleksandar.tsvetkov@sap.com>
2017-10-27 13:31:14 +0300

    Improve JdbcUtil
    Change-Id: I516fb678fa59b785ad8c4594db6f106598459c39

--- com.sap.cloud.lm.sl.persistence/src/main/java/com/sap/cloud/lm/sl/persistence/util/JdbcUtil.java
+++ com.sap.cloud.lm.sl.persistence/src/main/java/com/sap/cloud/lm/sl/persistence/util/JdbcUtil.java
@@ -1,38 +1,64 @@
 package com.sap.cloud.lm.sl.persistence.util;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sap.cloud.lm.sl.persistence.message.Messages;
 
 public class JdbcUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdbcUtil.class);
+
     public static void closeQuietly(ResultSet resultSet) {
+        if (resultSet == null) {
+            return;
+        }
         try {
-            if (resultSet != null) {
-                resultSet.close();
-            }
-        } catch (Exception ignored) {
-            // ignore
+            resultSet.close();
+        } catch (SQLException e) {
+            logSQLException(e);
+            LOGGER.warn(Messages.COULD_NOT_CLOSE_RESULT_SET, e);
+        } catch (Exception e) {
+            LOGGER.warn(Messages.COULD_NOT_CLOSE_RESULT_SET, e);
         }
     }
 
-    public static void closeQuietly(PreparedStatement statement) {
+    public static void closeQuietly(Statement statement) {
+        if (statement == null) {
+            return;
+        }
         try {
-            if (statement != null) {
-                statement.close();
-            }
-        } catch (Exception ignored) {
-            // ignore
+            statement.close();
+        } catch (SQLException e) {
+            logSQLException(e);
+            LOGGER.warn(Messages.COULD_NOT_CLOSE_STATEMENT, e);
+        } catch (Exception e) {
+            LOGGER.warn(Messages.COULD_NOT_CLOSE_STATEMENT, e);
         }
     }
 
     public static void closeQuietly(Connection connection) {
+        if (connection == null) {
+            return;
+        }
         try {
-            if (connection != null) {
-                connection.close();
-            }
-        } catch (Exception ignored) {
-            // ignore
+            connection.close();
+        } catch (SQLException e) {
+            logSQLException(e);
+            LOGGER.warn(Messages.COULD_NOT_CLOSE_CONNECTION, e);
+        } catch (Exception e) {
+            LOGGER.warn(Messages.COULD_NOT_CLOSE_CONNECTION, e);
+        }
+    }
+
+    public static void logSQLException(SQLException exception) {
+        for (Throwable sameLevelException : exception) {
+            LOGGER.warn(sameLevelException.getMessage(), sameLevelException);
         }
     }
 
