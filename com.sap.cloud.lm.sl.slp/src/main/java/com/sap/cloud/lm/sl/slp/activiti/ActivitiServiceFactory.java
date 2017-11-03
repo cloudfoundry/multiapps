99c864cb6450cebceb7698146d43ecd564fa2b18
Mihail Tsonev <mihail.tsonev@sap.com>
2017-11-03 16:24:50 +0200

    Remove SLP dependency

--- com.sap.cloud.lm.sl.slp/src/main/java/com/sap/cloud/lm/sl/slp/activiti/ActivitiServiceFactory.java
+++
@@ -1,8 +0,0 @@
-package com.sap.cloud.lm.sl.slp.activiti;
-
-import com.sap.cloud.lm.sl.slp.model.ServiceMetadata;
-
-public interface ActivitiServiceFactory {
-
-    ActivitiService createActivitiService(ServiceMetadata serviceMetadata);
-}
--- com.sap.cloud.lm.sl.slp/src/main/java/com/sap/cloud/lm/sl/slp/model/AbstractRoadmapStepMetadataBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.sap.cloud.lm.sl.slp.model;
-
-import com.sap.cloud.lm.sl.slp.model.builder.RoadmapStepMetadataBuilder;
-
-abstract class AbstractRoadmapStepMetadataBuilder<T extends RoadmapStepMetadata, BT extends RoadmapStepMetadataBuilder>
-    extends AbstractStepMetadataBuilder<T, BT>implements RoadmapStepMetadataBuilder {
-
-}
