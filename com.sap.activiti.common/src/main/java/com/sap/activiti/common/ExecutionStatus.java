e3de1c394be1340d98c2200a2d74afd1bed3a7c5
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-08-13 19:07:22 +0300

    Remove unused module (com.sap.activiti.common)

--- com.sap.activiti.common/src/main/java/com/sap/activiti/common/ExecutionStatus.java
+++
@@ -1,8 +0,0 @@
-package com.sap.activiti.common;
-
-public enum ExecutionStatus {
-    NEW, // Task/Service is not started
-    RUNNING, SUCCESS, FAILED, //
-    LOGICAL_RETRY, // Logical composite step has failed but may be retried
-    SKIPPED; // Task has been skipped by the operator
-}
