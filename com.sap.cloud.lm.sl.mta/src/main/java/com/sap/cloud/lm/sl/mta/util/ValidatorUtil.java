852b27e2219de98d461227aa146c50635aa108e9
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2018-08-15 13:57:28 +0300

    Remove unnecessary classes

--- com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/util/ValidatorUtil.java
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/util/ValidatorUtil.java
@@ -1,6 +1,6 @@
 package com.sap.cloud.lm.sl.mta.util;
 
-import static com.sap.cloud.lm.sl.common.util.CommonUtil.isNullOrEmpty;
+import org.apache.commons.lang3.StringUtils;
 
 import com.sap.cloud.lm.sl.common.ContentException;
 import com.sap.cloud.lm.sl.common.util.CommonUtil;
@@ -12,13 +12,13 @@ public class ValidatorUtil {
 
     public static void validateModifiableElements(String elementType, String elementPrefix, String containerName, String key, Object value,
         Object parentValue) throws ContentException {
-        if (!isNullOrEmpty(parentValue) && !parentValue.equals(value)) {
+        if (!CommonUtil.isNullOrEmpty(parentValue) && !parentValue.equals(value)) {
             throw new ContentException(Messages.CANNOT_MODIFY_ELEMENT, elementType, getPrefixedName(elementPrefix, key), containerName);
         }
     }
 
     public static String getPrefixedName(String prefix, String name, String separator) {
-        return (!CommonUtil.isNullOrEmpty(prefix) ? prefix + separator : "") + name;
+        return (!StringUtils.isEmpty(prefix) ? prefix + separator : "") + name;
     }
 
     public static String getPrefixedName(String prefix, String name) {
