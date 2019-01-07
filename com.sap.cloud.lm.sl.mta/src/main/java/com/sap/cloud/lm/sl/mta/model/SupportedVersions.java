a9cdfda469534d6048fa45ff8c52e5bd65b69f65
Ivanka Tashkova <ivanka.tashkova@sap.com>
2019-01-07 11:56:16 +0200

    Revert "remove 1.0 from supported versions"
    This reverts commit e716c4909ef19699f0240c3200e3e1ae34c5fcec.

--- com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/model/SupportedVersions.java
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/model/SupportedVersions.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class SupportedVersions {
 
-    public static final List<String> SUPPORTED_VERSIONS = Arrays.asList("2.0", "2.1", "2.2", "3.0", "3.1", "3.2");
+    public static final List<String> SUPPORTED_VERSIONS = Arrays.asList("1.0", "2.0", "2.1", "2.2", "3.0", "3.1", "3.2");
 
     public static boolean isSupported(Version schemaVersion) {
         return SUPPORTED_VERSIONS.stream()
