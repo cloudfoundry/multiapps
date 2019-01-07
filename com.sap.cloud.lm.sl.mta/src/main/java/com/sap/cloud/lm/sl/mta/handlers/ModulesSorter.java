c544e5e9fff17cdc18a468393b9dce07986bc3b9
Ivanka Tashkova <ivanka.tashkova@sap.com>
2019-01-07 11:56:16 +0200

    Revert "ModulesSorter"
    This reverts commit e7931d1c129e49704872171a4d3667cc2a62f325.

--- com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/handlers/ModulesSorter.java
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/handlers/ModulesSorter.java
@@ -2,8 +2,7 @@ package com.sap.cloud.lm.sl.mta.handlers;
 
 import java.util.List;
 
-import com.sap.cloud.lm.sl.mta.model.v2.Module;
-
+import com.sap.cloud.lm.sl.mta.model.v1.Module;
 
 public interface ModulesSorter {
 
--- com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/handlers/v2/ModulesSorter.java
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/handlers/v2/ModulesSorter.java
@@ -1,63 +1,24 @@
 package com.sap.cloud.lm.sl.mta.handlers.v2;
 
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.sap.cloud.lm.sl.mta.builders.v2.ModuleDependenciesCollector;
-import com.sap.cloud.lm.sl.mta.model.v2.DeploymentDescriptor;
-import com.sap.cloud.lm.sl.mta.model.v2.Module;
+import com.sap.cloud.lm.sl.mta.model.v1.DeploymentDescriptor;
 
-public class ModulesSorter implements com.sap.cloud.lm.sl.mta.handlers.ModulesSorter {
-
-    protected DeploymentDescriptor descriptor;
-    protected DescriptorHandler handler;
-    protected String dependencyTypeProperty;
-    protected String hardDependencyType;
+public class ModulesSorter extends com.sap.cloud.lm.sl.mta.handlers.v1.ModulesSorter {
 
     public ModulesSorter(DeploymentDescriptor descriptor, DescriptorHandler handler, String dependencyTypeProperty,
         String hardDependencyType) {
-        this.descriptor = descriptor;
-        this.handler = handler;
-        this.dependencyTypeProperty = dependencyTypeProperty;
-        this.hardDependencyType = hardDependencyType;
+        super(descriptor, handler, dependencyTypeProperty, hardDependencyType);
     }
-    
-    public List<? extends Module> sort() {
-        Map<Module, Set<String>> modulesAndDeploymentDependencies = getModulesAndDeploymentDependencies();
-        List<Entry<Module, Set<String>>> modulesAndDeploymentDependenciesSorted = modulesAndDeploymentDependencies.entrySet()
-            .stream()
-            .collect(Collectors.toList());
 
-        Collections.sort(modulesAndDeploymentDependenciesSorted, getModuleComparator(dependencyTypeProperty, hardDependencyType));
-
-        return modulesAndDeploymentDependenciesSorted.stream()
-            .map(Entry::getKey)
-            .collect(Collectors.toList());
-    }
-    
-    protected Map<Module, Set<String>> getModulesAndDeploymentDependencies() {
-        Map<Module, Set<String>> result = new LinkedHashMap<>();
-        for (Module module : descriptor.getModules()) {
-            result.put(module, getDependencies(module));
-        }
-        return result;
-    }
-    
-    protected Set<String> getDependencies(Module module) {
-        return getDependenciesCollector().collect(module);
-    }
-    
+    @Override
     protected ModuleDependenciesCollector getDependenciesCollector() {
         return new ModuleDependenciesCollector(descriptor, handler);
     }
 
-    protected Comparator<Entry<Module, Set<String>>> getModuleComparator(String dependencyTypeProperty, String hardDependencyType) {
+    @Override
+    protected Comparator getModuleComparator(String dependencyTypeProperty, String hardDependencyType) {
         return new ModuleComparator(dependencyTypeProperty, hardDependencyType);
     }
 
