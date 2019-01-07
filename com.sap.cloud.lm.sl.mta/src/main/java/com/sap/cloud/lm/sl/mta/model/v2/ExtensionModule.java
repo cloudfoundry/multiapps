30c9bec2232d8250f50c8cfaa4ff9595cf198a3f
Ivanka Tashkova <ivanka.tashkova@sap.com>
2019-01-07 11:56:16 +0200

    Revert "ExtensionModule"
    This reverts commit c9360f6f092f14a5d670f941872a46ba65de7eee.

--- com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/model/v2/ExtensionModule.java
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/model/v2/ExtensionModule.java
@@ -125,14 +125,6 @@ public class ExtensionModule implements VisitableElement, NamedElement, Properti
             return result;
         }
 
-        public void setName(String name) {
-            this.name = name;
-        }
-        
-        public void setProperties(Map<String, Object> properties) {
-            this.properties = properties;
-        }
-        
         public void setParameters(Map<String, Object> parameters) {
             this.parameters = parameters;
         }
