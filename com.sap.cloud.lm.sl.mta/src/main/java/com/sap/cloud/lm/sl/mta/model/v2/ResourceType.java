8f8142b47886d29868a9fb63a94209b19b657691
Ivanka Tashkova <ivanka.tashkova@sap.com>
2019-01-07 11:56:16 +0200

    Revert "PlatformResourceType"
    This reverts commit 3d2643f3350c260299ccb69ea3b0ae2d2876873c.

--- com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/model/v2/ResourceType.java
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/model/v2/ResourceType.java
@@ -7,28 +7,19 @@ import java.util.Map;
 import org.apache.commons.lang3.ObjectUtils;
 
 import com.sap.cloud.lm.sl.common.util.MapUtil;
-import com.sap.cloud.lm.sl.mta.model.ElementContext;
-import com.sap.cloud.lm.sl.mta.model.NamedElement;
 import com.sap.cloud.lm.sl.mta.model.ParametersContainer;
-import com.sap.cloud.lm.sl.mta.model.VisitableElement;
-import com.sap.cloud.lm.sl.mta.model.Visitor;
 
-public class ResourceType implements VisitableElement, NamedElement, ParametersContainer {
+public class ResourceType extends com.sap.cloud.lm.sl.mta.model.v1.ResourceType implements ParametersContainer {
 
-    private String name;
-    private String resourceManager;
     private Map<String, Object> parameters;
 
     protected ResourceType() {
 
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getResourceManager() {
-        return resourceManager;
+    @Override
+    public Map<String, Object> getProperties() {
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -36,12 +27,9 @@ public class ResourceType implements VisitableElement, NamedElement, ParametersC
         return MapUtil.unmodifiable(parameters);
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setResourceManager(String resourceManager) {
-        this.resourceManager = resourceManager;
+    @Override
+    public void setProperties(Map<String, Object> properties) {
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -49,18 +37,11 @@ public class ResourceType implements VisitableElement, NamedElement, ParametersC
         this.parameters = new LinkedHashMap<>(parameters);
     }
 
-    @Override
-    public void accept(ElementContext context, Visitor visitor) {
-        visitor.visit(context, this);
-    }
+    public static class Builder extends com.sap.cloud.lm.sl.mta.model.v1.ResourceType.Builder {
 
-    public static class Builder {
-
-        protected String name;
-        protected String resourceManager;
-        protected Map<String, Object> properties;
         protected Map<String, Object> parameters;
 
+        @Override
         public ResourceType build() {
             ResourceType result = new ResourceType();
             result.setName(name);
@@ -69,20 +50,13 @@ public class ResourceType implements VisitableElement, NamedElement, ParametersC
             return result;
         }
 
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setResourceManager(String resourceManager) {
-            this.resourceManager = resourceManager;
-        }
-
-        public void setProperties(Map<String, Object> properties) {
-            throw new UnsupportedOperationException();
-        }
-
         public void setParameters(Map<String, Object> parameters) {
             this.parameters = parameters;
+        }
+
+        @Override
+        public void setProperties(Map<String, Object> properties) {
+            throw new UnsupportedOperationException();
         }
 
     }
