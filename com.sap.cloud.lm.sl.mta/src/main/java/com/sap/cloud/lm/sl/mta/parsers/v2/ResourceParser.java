ec67c39f9fb2d16ee1a99e31d14a058c120ed2bf
Ivanka Tashkova <ivanka.tashkova@sap.com>
2019-01-07 11:56:16 +0200

    Revert "ResourceParser"
    This reverts commit cc24582441f2675e735cc629ce7a5f2080f0a32a.

--- com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/parsers/v2/ResourceParser.java
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/parsers/v2/ResourceParser.java
@@ -2,32 +2,23 @@ package com.sap.cloud.lm.sl.mta.parsers.v2;
 
 import static com.sap.cloud.lm.sl.mta.handlers.v2.Schemas.RESOURCE;
 
-import java.util.List;
 import java.util.Map;
 
 import com.sap.cloud.lm.sl.common.ParsingException;
 import com.sap.cloud.lm.sl.mta.model.v2.Resource;
 import com.sap.cloud.lm.sl.mta.model.v2.Resource.Builder;
-import com.sap.cloud.lm.sl.mta.parsers.ModelParser;
 import com.sap.cloud.lm.sl.mta.schema.MapElement;
 
-public class ResourceParser extends ModelParser<Resource> {
+public class ResourceParser extends com.sap.cloud.lm.sl.mta.parsers.v1.ResourceParser {
 
-    protected static final String PROCESSED_OBJECT_NAME = "MTA resource";
-
-    public static final String NAME = "name";
-    public static final String TYPE = "type";
-    public static final String DESCRIPTION = "description";
-    public static final String GROUPS = "groups";
-    public static final String PROPERTIES = "properties";
     public static final String PARAMETERS = "parameters";
 
     public ResourceParser(Map<String, Object> source) {
-        this(RESOURCE, source);
+        super(RESOURCE, source);
     }
 
     protected ResourceParser(MapElement schema, Map<String, Object> source) {
-        super(PROCESSED_OBJECT_NAME, schema, source);
+        super(schema, source);
     }
 
     @Override
@@ -39,26 +30,6 @@ public class ResourceParser extends ModelParser<Resource> {
         builder.setProperties(getProperties());
         builder.setParameters(getParameters());
         return builder.build();
-    }
-    
-    protected String getName() {
-        return getStringElement(NAME);
-    }
-
-    protected String getType() {
-        return getStringElement(TYPE);
-    }
-
-    protected String getDescription() {
-        return getStringElement(DESCRIPTION);
-    }
-
-    protected List<String> getGroups() {
-        return getListElement(GROUPS);
-    }
-
-    protected Map<String, Object> getProperties() {
-        return getMapElement(PROPERTIES);
     }
 
     protected Map<String, Object> getParameters() {
