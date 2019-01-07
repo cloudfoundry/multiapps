41ab65875d8b815041bf06b8b964c8bf4fc5cb77
Ivanka Tashkova <ivanka.tashkova@sap.com>
2019-01-07 11:56:16 +0200

    Revert "ResourceTypeParser"
    This reverts commit e5cc51695c45de9b7f7eb193910c8082d3e22e46.

---
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/parsers/v1/ResourceTypeParser.java
@@ -0,0 +1,50 @@
+package com.sap.cloud.lm.sl.mta.parsers.v1;
+
+import static com.sap.cloud.lm.sl.mta.handlers.v1.Schemas.RESOURCE_TYPE;
+
+import java.util.Map;
+
+import com.sap.cloud.lm.sl.common.ParsingException;
+import com.sap.cloud.lm.sl.mta.model.v1.ResourceType;
+import com.sap.cloud.lm.sl.mta.model.v1.ResourceType.Builder;
+import com.sap.cloud.lm.sl.mta.parsers.ModelParser;
+import com.sap.cloud.lm.sl.mta.schema.MapElement;
+
+public class ResourceTypeParser extends ModelParser<ResourceType> {
+
+    protected static final String PROCESSED_OBJECT_NAME = "MTA resource type";
+
+    public static final String PROPERTIES = "properties";
+    public static final String NAME = "name";
+    public static final String RESOURCE_MANAGER = "resource-manager";
+
+    public ResourceTypeParser(Map<String, Object> source) {
+        this(RESOURCE_TYPE, source);
+    }
+
+    protected ResourceTypeParser(MapElement schema, Map<String, Object> source) {
+        super(PROCESSED_OBJECT_NAME, schema, source);
+    }
+
+    @Override
+    public ResourceType parse() throws ParsingException {
+        Builder builder = new Builder();
+        builder.setName(getName());
+        builder.setResourceManager(getResourceManager());
+        builder.setProperties(getProperties());
+        return builder.build();
+    }
+
+    protected String getName() {
+        return getStringElement(NAME);
+    }
+
+    protected String getResourceManager() {
+        return getStringElement(RESOURCE_MANAGER);
+    }
+
+    protected Map<String, Object> getProperties() {
+        return getMapElement(PROPERTIES);
+    }
+
+}
--- com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/parsers/v2/ResourceTypeParser.java
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/parsers/v2/ResourceTypeParser.java
@@ -7,24 +7,18 @@ import java.util.Map;
 import com.sap.cloud.lm.sl.common.ParsingException;
 import com.sap.cloud.lm.sl.mta.model.v2.ResourceType;
 import com.sap.cloud.lm.sl.mta.model.v2.ResourceType.Builder;
-import com.sap.cloud.lm.sl.mta.parsers.ModelParser;
 import com.sap.cloud.lm.sl.mta.schema.MapElement;
 
-public class ResourceTypeParser extends ModelParser<ResourceType> {
+public class ResourceTypeParser extends com.sap.cloud.lm.sl.mta.parsers.v1.ResourceTypeParser {
 
-    protected static final String PROCESSED_OBJECT_NAME = "MTA resource type";
-
-    public static final String NAME = "name";
-    public static final String PROPERTIES = "properties";
     public static final String PARAMETERS = "parameters";
-    public static final String RESOURCE_MANAGER = "resource-manager";
 
     public ResourceTypeParser(Map<String, Object> source) {
-        this(RESOURCE_TYPE, source);
+        super(RESOURCE_TYPE, source);
     }
 
     protected ResourceTypeParser(MapElement schema, Map<String, Object> source) {
-        super(PROCESSED_OBJECT_NAME, schema, source);
+        super(schema, source);
     }
 
     @Override
@@ -34,14 +28,6 @@ public class ResourceTypeParser extends ModelParser<ResourceType> {
         builder.setResourceManager(getResourceManager());
         builder.setParameters(getParameters());
         return builder.build();
-    }
-
-    protected String getName() {
-        return getStringElement(NAME);
-    }
-
-    protected String getResourceManager() {
-        return getStringElement(RESOURCE_MANAGER);
     }
 
     protected Map<String, Object> getParameters() {
