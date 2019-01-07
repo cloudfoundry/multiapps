e4e74ccc7fdcd4bb4f02a2741ea7d5580d5d8a4d
Ivanka Tashkova <ivanka.tashkova@sap.com>
2019-01-07 11:56:16 +0200

    Revert "PropertiesChainBuilder"
    This reverts commit 7d954e1a1fb7ffa7ad4359777437d6cc3a198a92.

--- com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/builders/v2/PropertiesChainBuilder.java
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/builders/v2/PropertiesChainBuilder.java
@@ -17,27 +17,21 @@ import com.sap.cloud.lm.sl.mta.model.v2.RequiredDependency;
 import com.sap.cloud.lm.sl.mta.model.v2.Resource;
 import com.sap.cloud.lm.sl.mta.util.PropertiesUtil;
 
-public class PropertiesChainBuilder {
-    
-    protected final DeploymentDescriptor descriptor;
-    protected final Platform platform;
-    protected final DescriptorHandler handler;
+public class PropertiesChainBuilder extends com.sap.cloud.lm.sl.mta.builders.v1.PropertiesChainBuilder {
+
+    public PropertiesChainBuilder(DeploymentDescriptor descriptor, Platform platform) {
+        super(descriptor, platform, new DescriptorHandler());
+    }
+
+    public PropertiesChainBuilder(DeploymentDescriptor descriptor) {
+        super(descriptor, null, new DescriptorHandler());
+    }
 
     public PropertiesChainBuilder(DeploymentDescriptor descriptor, Platform platform, DescriptorHandler handler) {
-        this.descriptor = descriptor;
-        this.platform = platform;
-        this.handler = handler;
-    }
-    
-    public PropertiesChainBuilder(DeploymentDescriptor descriptor, Platform platform) {
-        this(descriptor, platform, new DescriptorHandler());
-    }
-    
-    public PropertiesChainBuilder(DeploymentDescriptor descriptor) {
-
-        this(descriptor, null, new DescriptorHandler());
+        super(descriptor, platform, handler);
     }
 
+    @Override
     public List<Map<String, Object>> buildModuleChain(String moduleName) {
         Module module = (Module) handler.findModule(descriptor, moduleName);
         if (module == null) {
@@ -47,15 +41,8 @@ public class PropertiesChainBuilder {
         ModuleType moduleType = (ModuleType) getModuleType(module);
         return getPropertiesList(dependencies, module, moduleType);
     }
-    
-    
-    protected ModuleType getModuleType(Module module) {
-        if (platform == null) {
-            return null;
-        }
-        return handler.findModuleType(platform, module.getType());
-    }
 
+    @Override
     public List<Map<String, Object>> buildModuleChainWithoutDependencies(String moduleName) {
         Module module = (Module) handler.findModule(descriptor, moduleName);
         if (module == null) {
@@ -64,17 +51,19 @@ public class PropertiesChainBuilder {
         ModuleType moduleType = (ModuleType) getModuleType(module);
         return PropertiesUtil.getPropertiesList(module, moduleType);
     }
-    
-    public List<Map<String, Object>> buildResourceTypeChain(String resourceName) {
-        throw new UnsupportedOperationException();
-    }
 
+    @Override
     public List<Map<String, Object>> buildResourceChain(String resourceName) {
         Resource resource = (Resource) handler.findResource(descriptor, resourceName);
         if (resource == null) {
             return Collections.emptyList();
         }
         return PropertiesUtil.getPropertiesList(resource);
+    }
+
+    @Override
+    public List<Map<String, Object>> buildResourceTypeChain(String resourceName) {
+        throw new UnsupportedOperationException();
     }
 
     protected static List<Map<String, Object>> getPropertiesList(List<RequiredDependency> dependencies, Module module,
