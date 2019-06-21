e75904416ef1e0f83b14e2c17a20acbe42aa2853
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2019-06-21 17:14:54 +0300

    Improve error messages when parsing YAML streams

--- com.sap.cloud.lm.sl.common/src/main/java/com/sap/cloud/lm/sl/common/util/YamlUtil.java
+++ com.sap.cloud.lm.sl.common/src/main/java/com/sap/cloud/lm/sl/common/util/YamlUtil.java
@@ -1,9 +1,12 @@
 package com.sap.cloud.lm.sl.common.util;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.SafeConstructor;
@@ -17,39 +20,29 @@ public class YamlUtil {
 
     private static final int EXCEPTION_MESSAGE_YAML_LENGTH_LIMIT = 255;
 
-    @SuppressWarnings("unchecked")
+    public static Map<String, Object> convertYamlToMap(InputStream yamlStream) throws ParsingException {
+        String yaml = toString(yamlStream);
+        return convertYamlToMap(yaml);
+    }
+
     public static Map<String, Object> convertYamlToMap(String yaml) throws ParsingException {
         try {
-            return (Map<String, Object>) new Yaml(new YamlTaggedObjectsConstructor()).load(yaml);
+            return new Yaml(new YamlTaggedObjectsConstructor()).load(yaml);
         } catch (Exception e) {
             throw new ParsingException(e, constructParsingExceptionMessage(e, yaml));
         }
     }
 
-    @SuppressWarnings("unchecked")
-    public static Map<String, Object> convertYamlToMap(InputStream yaml) throws ParsingException {
-        try {
-            return (Map<String, Object>) new Yaml(new YamlTaggedObjectsConstructor()).load(yaml);
-        } catch (Exception e) {
-            throw new ParsingException(e, Messages.ERROR_PARSING_YAML_STREAM, e.getMessage());
-        }
+    public static List<Object> convertYamlToList(InputStream yamlStream) throws ParsingException {
+        String yaml = toString(yamlStream);
+        return convertYamlToList(yaml);
     }
 
-    @SuppressWarnings("unchecked")
     public static List<Object> convertYamlToList(String yaml) throws ParsingException {
         try {
-            return (List<Object>) new Yaml(new YamlTaggedObjectsConstructor()).load(yaml);
+            return new Yaml(new YamlTaggedObjectsConstructor()).load(yaml);
         } catch (Exception e) {
             throw new ParsingException(e, constructParsingExceptionMessage(e, yaml));
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public static List<Object> convertYamlToList(InputStream yaml) throws ParsingException {
-        try {
-            return (List<Object>) new Yaml(new YamlTaggedObjectsConstructor()).load(yaml);
-        } catch (Exception e) {
-            throw new ParsingException(e, Messages.ERROR_PARSING_YAML_STREAM, e.getMessage());
         }
     }
 
@@ -61,6 +54,14 @@ public class YamlUtil {
     private static String constructParsingExceptionMessage(Exception e, String yaml) {
         String trimmedYaml = StringUtils.abbreviate(yaml, EXCEPTION_MESSAGE_YAML_LENGTH_LIMIT);
         return String.format(Messages.ERROR_PARSING_YAML_STRING, trimmedYaml, e.getMessage());
+    }
+
+    private static String toString(InputStream yamlStream) {
+        try {
+            return IOUtils.toString(yamlStream, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new ParsingException(e, Messages.ERROR_PARSING_YAML_STREAM, e.getMessage());
+        }
     }
 
 }
