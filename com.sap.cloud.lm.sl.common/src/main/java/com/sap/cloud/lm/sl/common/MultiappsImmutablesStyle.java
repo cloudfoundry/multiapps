7d927321521a76162d85a37c38c5aff1c8f48977
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-04-01 11:12:36 +0300

    Add support for Immutables
    See https://immutables.github.io/

---
+++ com.sap.cloud.lm.sl.common/src/main/java/com/sap/cloud/lm/sl/common/MultiappsImmutablesStyle.java
@@ -0,0 +1,12 @@
+package com.sap.cloud.lm.sl.common;
+
+import org.immutables.value.Value;
+
+/**
+ * Used for generation of code via <a href="https://immutables.github.io/">Immutables</a>.
+ *
+ */
+@Value.Style(depluralize = true, jdkOnly = true)
+public @interface MultiappsImmutablesStyle {
+
+}
---
+++ com.sap.cloud.lm.sl.mta/src/main/java/com/sap/cloud/lm/sl/mta/package-info.java
@@ -0,0 +1,4 @@
+@MultiappsImmutablesStyle
+package com.sap.cloud.lm.sl.mta;
+
+import com.sap.cloud.lm.sl.common.MultiappsImmutablesStyle;
