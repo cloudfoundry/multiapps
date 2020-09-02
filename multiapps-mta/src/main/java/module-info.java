f008c7e4f5f591ef8b97cf3cf0ec16df5930d493
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-09-02 18:06:52 +0300

    Add module-info to multiapps-mta
    JIRA: LMCROSSITXSADEPLOY-2171

---
+++ multiapps-mta/src/main/java/module-info.java
@@ -0,0 +1,34 @@
+open module org.cloudfoundry.multiapps.mta {
+
+    exports org.cloudfoundry.multiapps.mta;
+    exports org.cloudfoundry.multiapps.mta.builders;
+    exports org.cloudfoundry.multiapps.mta.builders.v2;
+    exports org.cloudfoundry.multiapps.mta.builders.v3;
+    exports org.cloudfoundry.multiapps.mta.handlers;
+    exports org.cloudfoundry.multiapps.mta.handlers.v2;
+    exports org.cloudfoundry.multiapps.mta.handlers.v3;
+    exports org.cloudfoundry.multiapps.mta.helpers;
+    exports org.cloudfoundry.multiapps.mta.mergers;
+    exports org.cloudfoundry.multiapps.mta.mergers.v2;
+    exports org.cloudfoundry.multiapps.mta.mergers.v3;
+    exports org.cloudfoundry.multiapps.mta.model;
+    exports org.cloudfoundry.multiapps.mta.parsers;
+    exports org.cloudfoundry.multiapps.mta.parsers.v2;
+    exports org.cloudfoundry.multiapps.mta.parsers.v3;
+    exports org.cloudfoundry.multiapps.mta.resolvers;
+    exports org.cloudfoundry.multiapps.mta.resolvers.v2;
+    exports org.cloudfoundry.multiapps.mta.resolvers.v3;
+    exports org.cloudfoundry.multiapps.mta.schema;
+    exports org.cloudfoundry.multiapps.mta.util;
+    exports org.cloudfoundry.multiapps.mta.validators;
+    exports org.cloudfoundry.multiapps.mta.validators.v2;
+    exports org.cloudfoundry.multiapps.mta.validators.v3;
+
+    requires transitive org.cloudfoundry.multiapps.common;
+
+    requires org.apache.commons.collections4;
+    requires org.apache.commons.io;
+    requires org.apache.commons.lang3;
+    requires semver4j;
+
+}
