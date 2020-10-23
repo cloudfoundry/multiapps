8569eba4c41e03b5826ca78db45a79959c13c564
Ivan Kasarov <ivan.kasarov@sap.com>
2020-10-23 16:31:41 +0300

    extending Test.Expectation.Type

--- multiapps-common-test/src/main/java/org/cloudfoundry/multiapps/common/test/Tester.java
+++ multiapps-common-test/src/main/java/org/cloudfoundry/multiapps/common/test/Tester.java
@@ -5,11 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.text.MessageFormat;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.function.FailableCallable;
 import org.apache.commons.lang3.function.FailableRunnable;
 import org.cloudfoundry.multiapps.common.util.JsonUtil;
+import org.junit.jupiter.api.Assertions;
 
 public class Tester {
 
@@ -35,25 +39,56 @@ public class Tester {
         }
     }
 
+    @SuppressWarnings("unchecked")
     private void validateSuccess(Expectation expectation, Object result) {
         assertTrue(expectation.expectsSuccess(), "Expected an exception, but the test finished successfully!");
 
         if (expectation.type == Expectation.Type.STRING) {
-            String expected = expectation.expectation;
-            String actual = Objects.toString(result, null);
-            assertEquals(expected, actual);
+            assertEquals(expectation.getExpectationAsString(), Objects.toString(result, null));
             return;
         }
-        Object expected = loadResourceAsJsonObject(expectation.expectation);
+
+        if (expectation.type == Expectation.Type.SET) {
+            validateSuccessForSetExpectation((Set<Object>) expectation.expectation, (Set<Object>) result);
+            return;
+        }
+
+        Object expected = loadResourceAsJsonObject(expectation.getExpectationAsString());
         Object actual = toJsonObject(result);
         // If they're not equal, we want to compare them as JSON strings, because then the differences would be easier to see. That's
         // why we're not using assertEquals here.
         if (actual.equals(expected)) {
             return;
         }
-        expected = loadResourceAsString(expectation.expectation);
+        expected = loadResourceAsString(expectation.getExpectationAsString());
         actual = toJson(result);
         assertEquals(expected, actual);
+    }
+
+    private void validateSuccessForSetExpectation(Set<Object> expectedSet, Set<Object> resultSet) {
+        if (expectedSet == null || resultSet == null) {
+            Assertions.assertTrue(expectedSet == null && resultSet == null,
+                                  "Only " + (expectedSet == null ? "EXPECTED" : "RESULT") + " set is null.");
+        } else {
+            List<Object> expectedButMissing = expectedSet.stream()
+                                                         .filter(element -> !resultSet.contains(element))
+                                                         .collect(Collectors.toList());
+            List<Object> notExpectedButPresent = resultSet.stream()
+                                                          .filter(element -> !expectedSet.contains(element))
+                                                          .collect(Collectors.toList());
+            String failMessage = "";
+            if (expectedButMissing.size() > 0) {
+                failMessage += String.format("EXPECTED set %s contains elements not present in RESULT %s : %s\n", expectedSet, resultSet,
+                                             expectedButMissing);
+            }
+            if (notExpectedButPresent.size() > 0) {
+                failMessage += String.format("RESULT set %s contains elements not present in EXPECTED %s : %s\n", resultSet, expectedSet,
+                                             notExpectedButPresent);
+            }
+            if (!failMessage.isEmpty()) {
+                Assertions.fail(failMessage);
+            }
+        }
     }
 
     private void validateFailure(Expectation expectation, Exception e) {
@@ -62,9 +97,9 @@ public class Tester {
             fail("Test failed: " + e.toString());
         }
         String exceptionMessage = e.getMessage();
-        assertTrue(exceptionMessage.contains(expectation.expectation),
-                   MessageFormat.format("Exception's message doesn't match up! Expected [{0}] to contain [{1}]!", expectation.expectation,
-                                        exceptionMessage));
+        assertTrue(exceptionMessage.contains(expectation.getExpectationAsString()),
+                   MessageFormat.format("Exception's message doesn't match up! Expected [{0}] to contain [{1}]!",
+                                        expectation.getExpectationAsString(), exceptionMessage));
     }
 
     private Object loadResourceAsJsonObject(String resource) {
@@ -100,23 +135,23 @@ public class Tester {
     public static class Expectation {
 
         public enum Type {
-            STRING, JSON, EXCEPTION, SKIP
+            STRING, JSON, EXCEPTION, SKIP, SET
         }
 
         private final Type type;
-        private final String expectation;
+        private final Object expectation;
 
         public Expectation(String expectation) {
             this(Type.STRING, expectation);
         }
 
-        public Expectation(Type type, String expectation) {
+        public Expectation(Type type, Object expectation) {
             this.type = type;
             this.expectation = expectation;
         }
 
         private boolean expectsSuccess() {
-            return type == Type.STRING || type == Type.JSON;
+            return type == Type.STRING || type == Type.JSON || type == Type.SET;
         }
 
         private boolean expectsFailure() {
@@ -125,6 +160,10 @@ public class Tester {
 
         private boolean shouldSkipTest() {
             return type == Type.SKIP;
+        }
+
+        private String getExpectationAsString() {
+            return (String) expectation;
         }
 
         public String toString() {
