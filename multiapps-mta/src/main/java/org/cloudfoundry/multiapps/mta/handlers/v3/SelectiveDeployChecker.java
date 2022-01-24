1404b7327b99b74f60faafd5029e5f84a9e358b9
abdermendz <abil.dermendzhiev@sap.com>
2022-01-24 17:50:03 +0200

    Edit SelectiveDeployChecker to validate only active resources

--- multiapps-mta/src/main/java/org/cloudfoundry/multiapps/mta/handlers/v3/SelectiveDeployChecker.java
+++ multiapps-mta/src/main/java/org/cloudfoundry/multiapps/mta/handlers/v3/SelectiveDeployChecker.java
@@ -3,13 +3,22 @@ package org.cloudfoundry.multiapps.mta.handlers.v3;
 import org.apache.commons.collections4.CollectionUtils;
 import org.cloudfoundry.multiapps.common.ContentException;
 import org.cloudfoundry.multiapps.mta.Messages;
+import org.cloudfoundry.multiapps.mta.handlers.v2.DescriptorHandler;
+import org.cloudfoundry.multiapps.mta.model.DeploymentDescriptor;
 import org.cloudfoundry.multiapps.mta.model.Resource;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SelectiveDeployChecker extends org.cloudfoundry.multiapps.mta.handlers.v2.SelectiveDeployChecker {
+
+    private final DeploymentDescriptor deploymentDescriptor;
+    private final DescriptorHandler descriptorHandler;
+
+    public SelectiveDeployChecker(DeploymentDescriptor deploymentDescriptor, DescriptorHandler descriptorHandler) {
+        this.descriptorHandler = descriptorHandler;
+        this.deploymentDescriptor = deploymentDescriptor;
+    }
 
     @Override
     public void check(List<Resource> resourcesForDeployment) {
@@ -33,7 +42,9 @@ public class SelectiveDeployChecker extends org.cloudfoundry.multiapps.mta.handl
 
     private void checkDependentResourceNames(Resource resource, List<String> allResourceNames) {
         for (String dependentResourceName : resource.getProcessedAfter()) {
-            if (!allResourceNames.contains(dependentResourceName)) {
+            if (descriptorHandler.findResource(deploymentDescriptor, dependentResourceName)
+                                 .isActive()
+                && !allResourceNames.contains(dependentResourceName)) {
                 throw new ContentException(Messages.INAPPROPRIATE_USE_OF_SELECTIVE_DEPLOY, resource.getName());
             }
         }
