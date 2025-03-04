a5b59bda99de7010a8f234a3616c8a06a2e882ae
Stefan Yonkov <mrstefan.yonkov@gmail.com>
2025-03-04 10:07:11 +0200

    bump dependencies, fix failing unit tests

--- multiapps-mta/src/test/java/org/cloudfoundry/multiapps/mta/builders/v2/PropertiesChainBuilderTest.java
+++ multiapps-mta/src/test/java/org/cloudfoundry/multiapps/mta/builders/v2/PropertiesChainBuilderTest.java
@@ -1,126 +1,55 @@
 package org.cloudfoundry.multiapps.mta.builders.v2;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import org.cloudfoundry.multiapps.common.test.Tester;
 import org.cloudfoundry.multiapps.common.test.Tester.Expectation;
-import org.cloudfoundry.multiapps.mta.MtaTestUtil;
-import org.cloudfoundry.multiapps.mta.handlers.v2.DescriptorParser;
-import org.cloudfoundry.multiapps.mta.model.DeploymentDescriptor;
-import org.cloudfoundry.multiapps.mta.model.Module;
-import org.cloudfoundry.multiapps.mta.model.Platform;
-import org.cloudfoundry.multiapps.mta.model.Resource;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class PropertiesChainBuilderTest {
+class PropertiesChainBuilderTest extends AbstractChainBuilderTest{
 
     protected final Tester tester = Tester.forClass(getClass());
 
-    static Stream<Arguments> testBuildModuleChain() {
-        return Stream.of(Arguments.of("mtad-01.yaml", "platform-01.json", new Expectation(Expectation.Type.JSON, "module-chain-04.json")),
-                         Arguments.of("mtad-01.yaml", "platform-02.json", new Expectation(Expectation.Type.JSON, "module-chain-05.json")),
-                         Arguments.of("mtad-01.yaml", "platform-03.json", new Expectation(Expectation.Type.JSON, "module-chain-06.json")));
-    }
-
-    static Stream<Arguments> testBuildModuleChainWithoutDependencies() {
-        return Stream.of(Arguments.of("mtad-01.yaml", "platform-01.json",
-                                      new Expectation(Expectation.Type.JSON, "module-chain-without-dependencies-04.json")),
-                         Arguments.of("mtad-01.yaml", "platform-02.json",
-                                      new Expectation(Expectation.Type.JSON, "module-chain-without-dependencies-05.json")),
-                         Arguments.of("mtad-01.yaml", "platform-03.json",
-                                      new Expectation(Expectation.Type.JSON, "module-chain-without-dependencies-06.json")));
-    }
-
-    static Stream<Arguments> testBuildResourceChain() {
-        return Stream.of(Arguments.of("mtad-01.yaml", "platform-01.json", new Expectation(Expectation.Type.JSON, "resource-chain-01.json")),
-                         Arguments.of("mtad-01.yaml", "platform-02.json", new Expectation(Expectation.Type.JSON, "resource-chain-06.json")),
-                         Arguments.of("mtad-01.yaml", "platform-03.json",
-                                      new Expectation(Expectation.Type.JSON, "resource-chain-03.json")));
-    }
-
     @ParameterizedTest
-    @MethodSource
+    @MethodSource("moduleChainSource")
     void testBuildModuleChain(String deploymentDescriptorLocation, String platformLocation, Expectation expectation) {
-        DeploymentDescriptor deploymentDescriptor = parseDeploymentDescriptor(deploymentDescriptorLocation);
-        Platform platform = parsePlatform(platformLocation);
+        executeTestBuildModuleChain(tester, deploymentDescriptorLocation, platformLocation, expectation);
+    }
 
-        PropertiesChainBuilder builder = createPropertiesChainBuilder(deploymentDescriptor, platform);
-        tester.test(() -> {
-            List<List<Map<String, Object>>> moduleChains = new ArrayList<>();
-            for (String moduleName : getModuleNames(deploymentDescriptor)) {
-                moduleChains.add(builder.buildModuleChain(moduleName));
-            }
-            return moduleChains;
-        }, expectation);
+    static Stream<Arguments> moduleChainSource() {
+        return Stream.of(Arguments.of("mtad-01.yaml", "platform-01.json", new Expectation(Expectation.Type.JSON, "module-chain-04.json")),
+                Arguments.of("mtad-01.yaml", "platform-02.json", new Expectation(Expectation.Type.JSON, "module-chain-05.json")),
+                Arguments.of("mtad-01.yaml", "platform-03.json", new Expectation(Expectation.Type.JSON, "module-chain-06.json")));
     }
 
     @ParameterizedTest
-    @MethodSource
+    @MethodSource("moduleChainWithoutDependenciesSource")
     void testBuildModuleChainWithoutDependencies(String deploymentDescriptorLocation, String platformLocation, Expectation expectation) {
-        DeploymentDescriptor deploymentDescriptor = parseDeploymentDescriptor(deploymentDescriptorLocation);
-        Platform platform = parsePlatform(platformLocation);
+        executeTestBuildModuleChainWithoutDependencies(tester, deploymentDescriptorLocation, platformLocation, expectation);
+    }
 
-        PropertiesChainBuilder builder = createPropertiesChainBuilder(deploymentDescriptor, platform);
-        tester.test(() -> {
-            List<List<Map<String, Object>>> moduleChains = new ArrayList<>();
-            for (String moduleName : getModuleNames(deploymentDescriptor)) {
-                moduleChains.add(builder.buildModuleChainWithoutDependencies(moduleName));
-            }
-            return moduleChains;
-        }, expectation);
+    static Stream<Arguments> moduleChainWithoutDependenciesSource() {
+        return Stream.of(Arguments.of("mtad-01.yaml", "platform-01.json",
+                        new Expectation(Expectation.Type.JSON, "module-chain-without-dependencies-04.json")),
+                Arguments.of("mtad-01.yaml", "platform-02.json",
+                        new Expectation(Expectation.Type.JSON, "module-chain-without-dependencies-05.json")),
+                Arguments.of("mtad-01.yaml", "platform-03.json",
+                        new Expectation(Expectation.Type.JSON, "module-chain-without-dependencies-06.json")));
     }
 
     @ParameterizedTest
-    @MethodSource
+    @MethodSource("resourceChainSource")
     void testBuildResourceChain(String deploymentDescriptorLocation, String platformLocation, Expectation expectation) {
-        DeploymentDescriptor deploymentDescriptor = parseDeploymentDescriptor(deploymentDescriptorLocation);
-        Platform platform = parsePlatform(platformLocation);
-
-        PropertiesChainBuilder builder = createPropertiesChainBuilder(deploymentDescriptor, platform);
-        tester.test(() -> {
-            List<List<Map<String, Object>>> resourceChains = new ArrayList<>();
-            for (String resourceName : getResourceNames(deploymentDescriptor)) {
-                resourceChains.add(builder.buildResourceChain(resourceName));
-            }
-            return resourceChains;
-        }, expectation);
+        executeTestBuildResourceChain(tester, deploymentDescriptorLocation, platformLocation, expectation);
     }
 
-    protected DeploymentDescriptor parseDeploymentDescriptor(String deploymentDescriptorLocation) {
-        return MtaTestUtil.loadDeploymentDescriptor(deploymentDescriptorLocation, createDescriptorParser(), getClass());
-    }
-
-    protected Platform parsePlatform(String platformLocation) {
-        return MtaTestUtil.loadPlatform(platformLocation, getClass());
-    }
-
-    protected List<String> getResourceNames(DeploymentDescriptor deploymentDescriptor) {
-        List<String> resourceNames = new ArrayList<>();
-        for (Resource resource : deploymentDescriptor.getResources()) {
-            resourceNames.add(resource.getName());
-        }
-        return resourceNames;
-    }
-
-    protected List<String> getModuleNames(DeploymentDescriptor deploymentDescriptor) {
-        List<String> moduleNames = new ArrayList<>();
-        for (Module module : deploymentDescriptor.getModules()) {
-            moduleNames.add(module.getName());
-        }
-        return moduleNames;
-    }
-
-    protected PropertiesChainBuilder createPropertiesChainBuilder(DeploymentDescriptor deploymentDescriptor, Platform platform) {
-        return new PropertiesChainBuilder(deploymentDescriptor, platform);
-    }
-
-    protected DescriptorParser createDescriptorParser() {
-        return new DescriptorParser();
+    static Stream<Arguments> resourceChainSource() {
+        return Stream.of(Arguments.of("mtad-01.yaml", "platform-01.json", new Expectation(Expectation.Type.JSON, "resource-chain-01.json")),
+                Arguments.of("mtad-01.yaml", "platform-02.json", new Expectation(Expectation.Type.JSON, "resource-chain-06.json")),
+                Arguments.of("mtad-01.yaml", "platform-03.json",
+                        new Expectation(Expectation.Type.JSON, "resource-chain-03.json")));
     }
 
 }
