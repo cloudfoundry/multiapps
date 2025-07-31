2abc977a6a6a4ae2ad903c48eeac91e1f8504ddb
theghost5800 <kristian0001@gmail.com>
2025-07-31 15:16:12 +0300

    Adapt immutables style to match cf-java-client-sap project

--- multiapps-common/src/main/java/org/cloudfoundry/multiapps/common/MultiappsImmutablesStyle.java
+++ multiapps-common/src/main/java/org/cloudfoundry/multiapps/common/MultiappsImmutablesStyle.java
@@ -4,9 +4,8 @@ import org.immutables.value.Value;
 
 /**
  * Used for generation of code via <a href="https://immutables.github.io/">Immutables</a>.
- *
  */
-@Value.Style(depluralize = true, jdkOnly = true, allowedClasspathAnnotations = java.lang.Override.class)
+@Value.Style(depluralize = true, jdkOnly = true, allowedClasspathAnnotations = java.lang.Override.class, visibility = Value.Style.ImplementationVisibility.PUBLIC, typeImmutableNested = "Immutable*")
 public @interface MultiappsImmutablesStyle {
 
 }
